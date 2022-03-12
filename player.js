ebfaeb5a250795e5f594c3b7cf5745d1b0030f7d
malte lindgren <malteecool@hotmail.com>
2022-03-12 15:26:30 +0100

    fix for height of divs issue

--- player.js
+++ player.js
@@ -1,16 +1,16 @@
-const width  = window.innerWidth || document.documentElement.clientWidth || 
-document.body.clientWidth;
-var height = (width * 9/16)
+const width = "100%"
+
+const height = "100%" //(width * 9/16)
 
 var embed = new Twitch.Embed("twitch-embed", {
     width: width,
     height: height,
-    channel: "Mande",
+    channel: "Dropped",
     layout: "video",
     autoplay: false
-  });
+});
 
-  embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
     var player = embed.getPlayer();
     player.play();
-  });
+});
